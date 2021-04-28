5b0a0bcc936d82f4f6c7c6e01184d5f4226a57bf
aqmalio <aqmal2399@gmail.com>
2021-04-28 21:13:13 +0700

    .

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{ fontSize: 30, color: '#ffffff' }}>Selamat Datang di</Text>
+        <Text style={{fontWeight: 'bold',fontSize: 30, color: '#ffffff'}}>SIMSTROKE</Text>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,6 +21,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 80
   },
+  header: {
+    backgroundColor: '#323ebe',
+    width: '90%',
+    padding: 40,
+    borderBottomEndRadius: 20,
+    alignSelf: 'flex-start',
+    fontSize: 50,
+    fontWeight: "900",
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 10
+    
+  }
 });
